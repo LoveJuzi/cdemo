298ddeace1bc6e8416e9b0c41fde41b6de612e09
LoveJuzi <mylovejuzi@icloud.com>
2021-01-12 12:03:27 +0800

    grep

--- C语言程序设计/4.函数与程序结构/01.grep/grep.c
+++ C语言程序设计/4.函数与程序结构/01.grep/grep.c
@@ -3,6 +3,10 @@
 #define MAXLEIN 1000
 
 int getline2(char s[], int lim);
+int strindex(char s[], char t[]);
+int strsub(char s[], char t[], int k);
+
+char pattern[] = "ould";
 
 int main()
 {
@@ -10,7 +14,10 @@ int main()
     // 读取所有行的方法
     while (getline2(s, MAXLEIN) > 0)
     {
-        printf("%s\n", s);
+        if (strindex(s, pattern) != -1)
+        {
+            printf("%s\n", s);
+        }
     }
 
     return 0;
@@ -44,4 +51,71 @@ int getline2(char s[], int lim)
     s[cnt] = '\0'; // 添加C语言的字符串的结束标志
 
     return cnt;
+}
+
+// s 是被匹配字符串
+// t 是模式串
+// 如果 t 在 s 中，返回 t 在 s 的索引
+// 如果不在 返回 -1
+int strindex(char s[], char t[])
+{
+    int k = 0;
+
+    for (;;)
+    {
+        if (s[k] == '\0')
+        {
+            break;
+        }
+
+        if (strsub(s, t, k) != -1)
+        {
+            return k;
+        }
+
+        k++;
+    }
+
+    return -1;
+}
+
+// 校验 t 是不是 s 的一个子串
+// k 是校验的位置
+// 如果是，返回t的长度
+// 如果不是，返回-1
+int strsub(char s[], char t[], int k)
+{
+    if (t[0] == '\0')
+    {
+        return -1;
+    }
+
+    int j = 0;
+    for (;;)
+    {
+        if (t[j] == '\0')
+        {
+            break;
+        }
+
+        if (s[k] == '\0')
+        {
+            break;
+        }
+
+        if (s[k] != t[j])
+        {
+            break;
+        }
+
+        k++;
+        j++;
+    }
+
+    if (t[j] == '\0')
+    {
+        return j;
+    }
+
+    return -1;
 }
