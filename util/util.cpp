116a6a21f0f79d9ed3cfa2c5f20e931a9dc770ed
LoveJuzi <mylovejuzi@icloud.com>
2021-03-28 10:32:03 +0800

    update

---
+++ util/util.cpp
@@ -0,0 +1,6 @@
+#include "util.h"
+
+int util::sum(int a, int b) {
+    return a + b;
+}
+
---
+++ util/util.h
@@ -0,0 +1,10 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+namespace util {
+
+int sum(int a, int b);
+
+}
+
+#endif
