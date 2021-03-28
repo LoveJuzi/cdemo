858efed8a442e546bbc9a509d0825c4b945bb90c
LoveJuzi <mylovejuzi@icloud.com>
2021-03-28 12:55:47 +0800

    hello

---
+++ helloworld/.vimrc
@@ -0,0 +1,2 @@
+
+let g:ycm_confirm_extra_conf=0  " 打开vim时不再询问是否加载ycm_extra_conf.py配置
---
+++ helloworld/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "util.h"
+
+int main(void) {
+    util::sum(1,2);
+    return 0;
+}
