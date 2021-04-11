97f10336372b3f5be5600b67c2acd04c3284bf99
LoveJuzi <mylovejuzi@icloud.com>
2021-04-11 12:41:44 +0800

    update

---
+++ 07_qtopengl/main.cpp
@@ -0,0 +1,12 @@
+#include "window.qt.h"
+#include <QApplication>
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+
+    Window w;
+    w.show();
+
+    return app.exec();
+}
+
